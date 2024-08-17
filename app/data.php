e16037f74eb4abcbc5075b02a9356a8f7fa9169a
DropSail <dropsaill@gmail.com>
2024-08-17 21:50:45 +0600

    Add files via upload

---
+++ app/data.php
@@ -0,0 +1,212 @@
+<?php
+
+$ThemeOptions = [
+
+   "header" => [
+
+                "contact" => [
+
+                    "email"         => "contactus@zitaly.com",
+                    "phone"         => "0245985522",
+                    "font"          => "arial",
+                ],
+                
+                "social_links" => [
+
+                    "twitter"       => '<i class="fa fa-twitter"></i>',
+                    "facebook"      => '<i class="fa fa-facebook"></i>',
+                    "linkedin"      => '<i class="fa fa-linkedin"></i>',
+                    "instagram"     => '<i class="fa fa-instagram"></i>',
+
+                ],
+
+                "logo" => [
+
+                    "logo_image"    => "images/logo.png",                
+                ],
+                
+                "site_title" => [
+
+                    "title_text"   => "Best Italian Food Restaurant",    
+                ]
+
+
+        ],
+
+
+    "nav_menu" => [
+
+        [
+            "menu_name"             => "Home",
+            "menu_link"             => "index.php",
+        ],
+        [
+            "menu_name"             => "Menu",
+            "menu_link"             => "menu.html",
+        ],
+        [
+            "menu_name"             => "Location",
+            "menu_link"             => "location.html",
+        ],
+        [
+            "menu_name"             => "Blog",
+            "menu_link"             => "archive.html",
+        ],
+        [
+            "menu_name"             => "Reservation",
+            "menu_link"             => "reservation.html",
+        ],
+        [
+            "menu_name"             => "Our Staff",
+            "menu_link"             => "staff.html",
+        ],
+        [
+            "menu_name"             => "News",
+            "menu_link"             => "news.html",
+        ],
+        [
+            "menu_name"             => "Gallery",
+            "menu_link"             => "gallery.html",
+        ],
+
+
+               
+            ],
+
+
+    "slider" => [
+
+                [
+
+                    "slider_image"  => "images/banner1.jpg",
+                    "slider_title"  => "We've got the best spareribs in town.",
+                    "slider_desc"   => "Nulla eget mauris quis elit mollis ornare vitae ut odio. Cras tincidunt, augue vitae sollicitudin commodo,quam elit varius est, et ornare ante massa quis tellus. Mauris nec lacinia nisl. ",
+
+                ],
+
+                [
+
+                    "slider_image"  => "images/banner2.jpg",
+                    "slider_title"  => "If food is an experience, then you'll find it at the restaurant.",
+                    "slider_desc"   => "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+
+                ],
+
+                [
+
+                    "slider_image"  => "images/banner3.jpg",
+                    "slider_title"  => "Enjoy our take-away menu.",
+                    "slider_desc"   => "At vero eos et accusam et justo duo dolores et ea rebum. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.",
+
+                ],
+
+
+
+
+
+            ],     
+    "slogan" => [
+
+                    "slogan_title"   => "Welcome to ZItaly Food",
+                    "slogan_desc"    => "Your awesome company slogan goes here, we have the best food around",
+
+
+    ],
+    
+    "welcome" => [
+
+                    "welcome_title"  => "Welcome",
+                    "welcome_desc"   => "Your awesome company slogan goes here, we have the best food around", 
+    ],
+
+
+    "boxitem" => [
+
+                [  "ribbon"         => "Fast Food",
+                    "boxitem_image" => "images/fast-food.jpg",
+                    "boxitem_desc"  => "The sliding menucard will surely impress your customers! Set up several pages and display them side by side, just as on a paper menucard!",
+                    "boxbtn"        => [
+
+                                   "boxbtn_title" => "Details",  
+                                   "boxbtn_link"  => "#",   
+                    ],
+                ],
+
+                [    
+                    "ribbon"         => "Menu Card",
+                    "boxitem_image" => "images/menu.jpg",
+                    "boxitem_desc"  => "The sliding menucard will surely impress your customers! Set up several pages and display them side by side, just as on a paper menucard!",
+                    "boxbtn"        => [
+
+                                   "boxbtn_title" => "Details",  
+                                   "boxbtn_link"  => "#",   
+                    ],
+                ],
+                
+                [
+
+                    "ribbon"         => "Reservation",
+                    "boxitem_image" => "images/reservation.jpg",
+                    "boxitem_desc"  => "The sliding menucard will surely impress your customers! Set up several pages and display them side by side, just as on a paper menucard!",
+                    "boxbtn"        => [
+
+                                   "boxbtn_title" => "Details",  
+                                   "boxbtn_link"  => "#",   
+                    ],
+
+                ],    
+
+                [    
+                    "ribbon"         => "Chef",
+                    "boxitem_image" => "images/chef.jpg",
+                    "boxitem_desc"  => "The sliding menucard will surely impress your customers! Set up several pages and display them side by side, just as on a paper menucard!",
+                    "boxbtn"        => [
+
+                                   "boxbtn_title" => "Details",  
+                                   "boxbtn_link"  => "#",   
+                    ],
+
+                ],
+                
+                [
+                    "ribbon"         => "Preview",
+                    "boxitem_image" => "images/preview.jpg",
+                    "boxitem_desc"  => "The sliding menucard will surely impress your customers! Set up several pages and display them side by side, just as on a paper menucard!",
+                    "boxbtn"        => [
+
+                                   "boxbtn_title" => "Details",  
+                                   "boxbtn_link"  => "#",   
+                    ],
+
+                ],
+                
+                
+                [
+                    "ribbon"         => "Text Heading",
+                    "boxitem_image" => "images/reservation.jpg",
+                    "boxitem_desc"  => "The sliding menucard will surely impress your customers! Set up several pages and display them side by side, just as on a paper menucard!",
+                    "boxbtn"        => [
+
+                                   "boxbtn_title" => "Details",  
+                                   "boxbtn_link"  => "#",   
+                    ],
+
+
+                ],   
+    ],
+
+
+
+    "design" => [
+
+                "font"              => "arial",
+                "background"        => "#0c8ac7"
+
+    ],
+
+
+
+    ];
+
+
+?>
